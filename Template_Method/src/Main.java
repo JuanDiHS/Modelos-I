9937a0b9e5f8cbe08012452d74980cabaee957b3
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2025-01-30 19:37:28 -0500

    Add files via upload

---
+++ Template_Method/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+public static void main(String[] args) {
+	EnemyTemplate guerrero = new Guerrero();
+	EnemyTemplate mago = new Mago();
+	guerrero.template();
+	mago.template();
+}
+}
