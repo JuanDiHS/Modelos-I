8fb7ed2b6f59e3dd5ef815fe90bee4c8e816ed65
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2025-02-04 18:26:40 -0500

    Delete Ejemplos/Template_Method directory

--- Ejemplos/Template_Method/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-public static void main(String[] args) {
-	EnemyTemplate guerrero = new Guerrero();
-	EnemyTemplate mago = new Mago();
-	guerrero.template();
-	mago.template();
-}
-}
