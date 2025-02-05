d3d0d161034efed7dd55256c27c8d0584667f268
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2025-02-04 19:05:30 -0500

    Delete Ejemplos/Visitor directory

--- Ejemplos/Visitor/src/Visitable.java
+++
@@ -1,5 +0,0 @@
-
-public interface Visitable {
-	void accept(Visitor visitor);
-
-}
--- Ejemplos/Visitor/src/Visitor.java
+++
@@ -1,7 +0,0 @@
-
-public interface Visitor {
-	void visit(Guerrero guerrero);
-	void visit(Mago mago);
-	void visit(Asesino asesino);
-
-}
